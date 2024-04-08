5c7f9343bd967917f4b9246b0d713df87e15e151
DogacUlgacc <ulgac1907@gmail.com>
2024-04-08 12:46:38 +0300

    Comment.js

---
+++ src/Components/Comment/Comment.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+function Comment() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [commentList, setCommentList] = useState([]);
+
+    useEffect(() => {
+        fetch("/comment")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setCommentList(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, [])
+
+    if (error) {
+        return <div>HATA!!</div>;
+    } else if (!isLoaded) {
+        return <div>Loading</div>;
+    } else {
+        return (
+            <ul>
+                {commentList.map(comment => (
+                    <li key={comment.id}>{comment.text}</li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
-import Post from './Components/Post';
+import Post from './Components/Post/Post.js';
+import Comment from './Components/Comment/Comment.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Post />
+  <Comment />
   </React.StrictMode>
 );
 
